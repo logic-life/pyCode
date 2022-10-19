fdf6fdb443e27af9567940599e645c015301590b
logic-life <zstmr@mail.ustc.edu.cn>
2022-10-20 00:04:33 +0800

    多线程下载斗破苍穹

--- novelWebsiteCrawler.py
+++ novelWebsiteCrawler.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Oct 19 16:47:48 2022
-
+Dest:多线程下载书籍
 @author: Logic-life
 """
 import requests as REQ
@@ -13,7 +13,7 @@ startUrl = 'https://www.doupobook.cc'
 blockPattern = '<div id="play_0">(.*)</a></li>'
 linkPattern = 'href="(.*?)" title='
 chapterNamePattern = 'title="(.*?)">'
-contentPattern = '<p>(.*?)</p>'
+contentPattern = '原站阅读。</p>(.*?)<div class'
 # %%获取html页面
 info = REQ.get('https://www.doupobook.cc/doupo/')
 info.encoding = "utf-8"
@@ -44,16 +44,30 @@ def getArticle(htmlUrl):
     return text
 # %% 
 def save(chapter,article):
-    os.makedirs('斗破苍穹',exist_ok=True)
-    with open(os.path.join('斗破苍穹',chapter+'.txt'),'w',encoding='utf-8') as f:
+    os.makedirs('斗破苍穹12',exist_ok=True)
+    with open(os.path.join('斗破苍穹12',chapter+'.txt'),'w',encoding='utf-8') as f:
         f.write(article)
-# %%
-# def opeator(pool):
-    # art = getArticle()
-    # save(chapter, article)
 # %% 获取章节名称，章节url
-# doupoChapterName,doupobookUrl = getTitleLink(html, blockPattern, linkPattern)
-# pool = Pool(5)
-
-# pool.map(getArticle,doupobookUrl)
-
+doupoChapterName,doupobookUrl = getTitleLink(html, blockPattern, linkPattern)
+book = [doupoChapterName[1:100],doupobookUrl[1:100]]
+book = [[row[i] for row in book] for i in range(len(book[0]))]
+# %% 循环的方式效率太低
+# for i in range(len(doupobookUrl)):
+#     article=getArticle(doupobookUrl[i])
+#     article = article.replace('</p><p>', '\n')
+#     article = article.replace('<p>', '')
+#     article = article.replace('</p>', '')
+#     article = article.replace('</div>', '')
+#     save(doupoChapterName[i],article)
+    
+# %% 下载书籍
+def saveTxt(book):
+    article=getArticle(book[1])
+    article = article.replace('</p><p>', '\n')
+    article = article.replace('<p>', '')
+    article = article.replace('</p>', '')
+    article = article.replace('</div>', '')
+    save(book[0],article)
+# %% 多线程下载章节，速度快
+pool = Pool(5)
+pool.map(saveTxt,book)
