62fd0977bd011c4d25cf5425c9a5a2a73c299856
logic-life <zstmr@mail.ustc.edu.cn>
2022-10-25 13:38:34 +0800

    修改存储路径

--- xpathSpyder.py
+++ xpathSpyder.py
@@ -14,7 +14,7 @@ import requests as REQ
 url = 'https://m.xbiquge.so/'
 content = REQ.get(url)
 content.encoding = 'GB2312'
-content = content.text;
+content = content.text
 novel = []
 author = []
 name = []
@@ -22,15 +22,18 @@ name = []
 selector = lxml.html.fromstring(content)
 url = selector.xpath('//div[@class="s_list"]/a/@href')
 text = selector.xpath('//div[@class="s_list"]/a/text()')
-for i in text :
-   i= str(i)
-   author.append(re.findall('(.*?)：', i)) 
-   name.append(re.findall('《(.*?)》', i)) 
+for i in text:
+    i = str(i)
+    author.append(re.findall('(.*?)：', i))
+    name.append(re.findall('《(.*?)》', i))
 # %%
 for i in range(len(author)):
-    novel.append([author[i][0],name[i][0],url[i]])
-# %% 
-infile = 'D:\code\小说信息.csv'
-with open(infile,'w+',newline='') as f:
+    novel.append([author[i][0], name[i][0], url[i]])
+# %%
+infile = '../spyderProject/小说信息.csv'
+print(infile)
+with open(infile, 'w+', newline='') as f:
     writer = csv.writer(f)
     writer.writerows(novel)
+
+# %%
